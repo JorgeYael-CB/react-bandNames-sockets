1228325cfec3e79f21bbe9c0977731a9f39a0af8
JorgeYael-CB <jorge.yael333@gmail.com>
2024-07-15 14:24:08 -0600

    agregada la coneccion y detectar cuando esta on/off

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
+import { useEffect, useState } from 'react'
 import { BandAdd, BandList } from './components'
+import io from 'socket.io-client'
 
+
+
+const connectionSocketServer = () => {
+  const socket = io.connect('http://localhost:8080', {
+    transports: ['websocket']
+  })
+  return socket;
+}
 
 
 function App() {
+  const [ socket ] = useState(connectionSocketServer())
+  const [Online, setOnline] = useState(false);
+
+
+  useEffect(() => {
+    setOnline( socket.connected );
+  }, [ socket ]);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setOnline(true)
+    })
+  }, [socket])
+
+  useEffect(() => {
+    socket.on('disconnect', () => {
+      setOnline(false)
+    })
+
+    // return socket.disconnect()
+  }, [socket])
+
 
   return (
     <div className="container">
       <div className="alert">
         <p>
-          Service status: 
-          <span className="text-success">Online</span>
-          <span className="text-danger">Offline</span>
+          Service status:
+          {
+            Online
+            ? <span className="text-success"> Online</span>
+            : <span className="text-danger"> Offline</span>
+          }
         </p>
       </div>
 
